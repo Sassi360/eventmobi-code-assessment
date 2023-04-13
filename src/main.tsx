f02284027f158c5ace59e12b6c0ac143a1f48f6b
Mohamed Sassi <mohamed@msassi.dev>
2023-04-13 17:11:20 -0400

    fix: update toggleColorMode path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { ChakraBaseProvider, ColorModeScript, theme } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
-import { ToggleColorMode } from './ToggleColorMode'
+import { ToggleColorMode } from './components/ToggleColorMode'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
